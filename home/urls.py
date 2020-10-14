dfe8748fac01d4ef65c3f369ddcddf1477d0eda6
MDRasel-Al-Mamun <djangoprojectbd@gmail.com>
2020-10-14 13:53:21 +0600

    jsonPesponse

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.homeView, name='home'),
+    path('tweets/', views.tweetListView, name='tweets'),
+    path('tweets/<str:id>/', views.tweetDetailView, name='tweet_detail_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from tweets.models import *
+from django.http import HttpResponse, Http404, JsonResponse
 
 
-def homeView(request):
+def homeView(request, *args, **kwargs):
     context = {}
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html', context,  status=200)
+
+
+def tweetListView(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+
+def tweetDetailView(request, id, * args, **kwargs):
+    data = {
+        'id': id,
+    }
+    status= 200
+    try:
+        obj = Tweet.objects.get(id=id)
+        data['content'] = obj.content
+    except:
+        data['message'] = 'Not Found'
+        status = 404
+    return JsonResponse(data, status=status)
